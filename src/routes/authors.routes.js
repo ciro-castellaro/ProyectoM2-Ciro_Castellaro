1d4013c57c0276659e8bfc8787b078de40fad93c
Ciro <cirocastellaro@gmail.com>
2026-06-16 15:51:27 -0300

    feat: agregar el middleware a las rutas

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const validateAuthor = require("../middlewares/validateAuthors");
 const authorsController = require("../controllers/authors.controller");
 
 router.get("/", authorsController.getAllAuthors);
 router.get("/:id", authorsController.getAuthorById);
-router.post("/", authorsController.createAuthor);
-router.put("/:id", authorsController.updateAuthor);
+router.post("/", validateAuthor, authorsController.createAuthor);
+router.put("/:id", validateAuthor, authorsController.updateAuthor);
 router.delete("/:id", authorsController.deleteAuthor);
 
 module.exports = router;
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const validatePost = require("../middlewares/validatePosts");
 const postsController = require("../controllers/posts.controller");
 
 router.get("/", postsController.getAllPosts);
 router.get("/author/:authorId", postsController.getPostsByAuthorId);
 router.get("/:id", postsController.getPostById);
-router.post("/", postsController.createPost);
-router.put("/:id", postsController.updatePost);
+router.post("/", validatePost, postsController.createPost);
+router.put("/:id", validatePost, postsController.updatePost);
 router.delete("/:id", postsController.deletePost);
 
 module.exports = router;
